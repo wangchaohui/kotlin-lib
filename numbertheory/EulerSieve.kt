db517e9a9b3ab68393108ba46782a549b8d52ce8
Chaohui Wang <fawkes.ecnu@gmail.com>
2024-02-03 23:44:58 +0800

    Create EulerSieve.kt

---
+++ numbertheory/EulerSieve.kt
@@ -0,0 +1,23 @@
+class EulerSieve(n: Int) {
+    private val isPrime = BitSet(n + 1)
+    val primes = mutableListOf<Int>()
+
+    init {
+        isPrime.set(2, n)
+        for (i in 2..n) {
+            if (isPrime[i]) primes += i
+            for (j in primes) {
+                if (i * j > n) break
+                isPrime.clear(i * j)
+                if (i % j == 0) break
+            }
+        }
+    }
+}
+
+// O(n) time & space
+
+val e = EulerSieve(1000)     //   168 primes
+val e = EulerSieve(10000)    //  1229 primes
+val e = EulerSieve(100000)   //  9592 primes
+val e = EulerSieve(1000000)  // 78498 primes
