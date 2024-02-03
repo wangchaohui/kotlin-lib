c35bffbc9aceeca99694a1d5c68e78088bfe563d
Chaohui Wang <fawkes.ecnu@gmail.com>
2024-02-03 23:47:44 +0800

    Update SimpleSegmentTree.kt

--- SimpleSegmentTree.kt
+++ SimpleSegmentTree.kt
@@ -27,7 +27,7 @@ class SimpleSegmentTree(private val n: Int) {
 
     private val t = Array(n * 2) { Interval() }
 
-    onstructor(values: List<Long>) : this(values.size) {
+    constructor(values: List<Long>) : this(values.size) {
         values.forEachIndexed { i, v -> t[i + n] = Interval(v) }
         for (i in n - 1 downTo 1) t[i] = Interval.combine(t[i * 2], t[i * 2 + 1])
     }
