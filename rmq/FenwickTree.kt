f3f726652d7d030a3f42b37528a87eb578481e22
Chaohui Wang <fawkes.ecnu@gmail.com>
2024-02-28 20:36:25 +0800

    Update FenwickTree.kt

--- rmq/FenwickTree.kt
+++ rmq/FenwickTree.kt
@@ -1,23 +1,31 @@
-typealias NumberType = Long
+class FenwickIntTree(n: Int) {
+    private val tree = IntArray(n)
 
-class FenwickTree(values: List<NumberType>) {
-    private val tree = values.toMutableList()
-
-    init {
-        tree.forEachIndexed { index, value ->
+    constructor(values: List<Int>) : this(values.size) {
+        values.forEachIndexed { index, value ->
             parent(index)?.let { tree[it] += value }
         }
     }
 
     /** Gets the sum of `[0, index]`. */
-    operator fun get(index: Int): NumberType {
+    fun prefixSum(index: Int): Int {
         check(index in tree.indices)
         return generateSequence(index, ::lastTree).sumOf(tree::get)
     }
 
-    fun add(index: Int, value: NumberType) {
+    /** Gets the sum of `[l, r]`. */
+    fun rangeSum(l: Int, r: Int): Int = prefixSum(r) - if (l > 0) prefixSum(l - 1) else 0
+
+    /** Gets the value at [index]. */
+    operator fun get(index: Int): Int = rangeSum(index, index)
+
+    fun add(index: Int, value: Int) {
         check(index in tree.indices)
         generateSequence(index, ::parent).forEach { tree[it] += value }
+    }
+
+    operator fun set(index: Int, value: Int) {
+        add(index, value - this[index])
     }
 
     private fun lastTree(i: Int) = (i - (i + 1).takeLowestOneBit()).takeIf { it >= 0 }
