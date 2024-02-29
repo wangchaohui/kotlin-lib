693ad5e070ced494040f86f8244be34c233928db
Chaohui Wang <fawkes.ecnu@gmail.com>
2024-03-01 00:46:00 +0800

    Update FenwickTree.kt

--- rmq/FenwickTree.kt
+++ rmq/FenwickTree.kt
@@ -1,30 +1,30 @@
-class FenwickIntTree(n: Int) {
-    private val tree = IntArray(n)
+class FenwickTree(n: Int) {
+    private val tree = LongArray(n)
 
-    constructor(values: List<Int>) : this(values.size) {
+    constructor(values: List<Long>) : this(values.size) {
         values.forEachIndexed { index, value ->
             parent(index)?.let { tree[it] += value }
         }
     }
 
     /** Gets the sum of `[0, index]`. */
-    fun prefixSum(index: Int): Int {
+    fun prefixSum(index: Int): Long {
         check(index in tree.indices)
         return generateSequence(index, ::lastTree).sumOf(tree::get)
     }
 
     /** Gets the sum of `[l, r]`. */
-    fun rangeSum(l: Int, r: Int): Int = prefixSum(r) - if (l > 0) prefixSum(l - 1) else 0
+    fun rangeSum(l: Int, r: Int): Long = prefixSum(r) - if (l > 0) prefixSum(l - 1) else 0
 
     /** Gets the value at [index]. */
-    operator fun get(index: Int): Int = rangeSum(index, index)
+    operator fun get(index: Int): Long = rangeSum(index, index)
 
-    fun add(index: Int, value: Int) {
+    fun add(index: Int, value: Long) {
         check(index in tree.indices)
         generateSequence(index, ::parent).forEach { tree[it] += value }
     }
 
-    operator fun set(index: Int, value: Int) {
+    operator fun set(index: Int, value: Long) {
         add(index, value - this[index])
     }
 
