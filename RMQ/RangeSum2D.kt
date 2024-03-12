8141118e4b8166c525746e856a4c1488b4a487d5
Chaohui Wang <chaohuiw@google.com>
2024-03-13 01:13:56 +0800

    RangeSum2D

--- RMQ/RangeSum2D.kt
+++ RMQ/RangeSum2D.kt
@@ -1,19 +1,37 @@
 class RangeSum2D(values: List<List<Int>>) {
     private val n = values.size
     private val m = values[0].size
-
     private val sum = Array(n) { LongArray(m) }
-
     private fun sum(i: Int, j: Int) = sum.getOrNull(i)?.getOrNull(j) ?: 0
+
+    fun rangeSum(bottom: Int, right: Int): Long {
+        check(bottom in 0..n && right in 0..m)
+        return sum(bottom - 1, right - 1)
+    }
 
     /** Queries the sum of the range. */
     fun rangeSum(top: Int, left: Int, bottom: Int, right: Int): Long {
         check(top in 0..bottom && bottom <= n)
         check(left in 0..right && right <= m)
-        return sum(bottom - 1, right - 1) -
-                sum(top - 1, right - 1) -
-                sum(bottom - 1, left - 1) +
-                sum(top - 1, left - 1)
+        return rangeSum(bottom, right) - rangeSum(top, right) - rangeSum(bottom, left) + rangeSum(top, left)
+    }
+
+    fun rangeSumTile(bottom: Int, right: Int): Long {
+        check(bottom >= 0 && right >= 0)
+        if (bottom <= n && right <= m) return rangeSum(bottom, right)
+        val a = bottom / n
+        val i = bottom % n
+        val b = right / m
+        val j = right % m
+        return rangeSum(n, m) * a * b + rangeSum(i, m) * b + rangeSum(n, j) * a + rangeSum(i, j)
+    }
+
+    fun rangeSumTile(top: Int, left: Int, bottom: Int, right: Int): Long {
+        check(top in 0..bottom && left in 0..right)
+        return rangeSumTile(bottom, right) -
+                rangeSumTile(top, right) -
+                rangeSumTile(bottom, left) +
+                rangeSumTile(top, left)
     }
 
     init {
