f9a5d984e0dae7a600adaaf41732920836b68f8e
Chaohui Wang <chaohuiw@google.com>
2024-03-23 22:03:06 +0800

    BinarySearch

--- BinarySearch.kt
+++ BinarySearch.kt
@@ -30,6 +30,38 @@ fun upperBound(low: Int, high: Int, predicate: IntPredicate): Int? {
     return l.takeIf(predicate::test)
 }
 
+/** The lower bound of [low, high], which `predicate.test(it)` is true. */
+fun lowerBound(low: Long, high: Long, predicate: LongPredicate): Long? {
+    if (low > high) return null
+    var l = low
+    var h = high
+    while (l < h) {
+        val m = (l + h) / 2
+        if (predicate.test(m)) {
+            h = m
+        } else {
+            l = m + 1
+        }
+    }
+    return l.takeIf(predicate::test)
+}
+
+/** The upper bound of [low, high], which `predicate.test(it)` is true. */
+fun upperBound(low: Long, high: Long, predicate: LongPredicate): Long? {
+    if (low > high) return null
+    var l = low
+    var h = high
+    while (l < h) {
+        val m = (l + h + 1) / 2
+        if (predicate.test(m)) {
+            l = m
+        } else {
+            h = m - 1
+        }
+    }
+    return l.takeIf(predicate::test)
+}
+
 /** The upper bound of [low, high], which `predicate.test(it)` is true. */
 fun upperBound(low: Double, high: Double, predicate: DoublePredicate): Double? {
     if (low > high) return null
