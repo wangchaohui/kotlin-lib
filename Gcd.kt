e0502fd404c16552e7a7e485fc45538f20bf9262
Chaohui Wang <fawkes.ecnu@gmail.com>
2024-04-18 00:50:04 +0800

    gcd

--- Gcd.kt
+++ Gcd.kt
@@ -2,3 +2,8 @@ tailrec fun gcd(a: Int, b: Int): Int {
     if (b == 0) return a
     return gcd(b, a % b)
 }
+
+tailrec fun gcd(a: Long, b: Long): Long {
+    if (b == 0L) return a
+    return gcd(b, a % b)
+}
