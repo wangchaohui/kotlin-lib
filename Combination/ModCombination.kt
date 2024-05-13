a2c1b4e9c0a02a9ae700ea014f788bb2bb23e4c5
Chaohui Wang <fawkes.ecnu@gmail.com>
2024-05-14 01:20:01 +0800

    ModCombination

--- Combination/ModCombination.kt
+++ Combination/ModCombination.kt
@@ -1,3 +1,41 @@
+@JvmInline
+value class ModInt private constructor(val value: Int) {
+    operator fun plus(other: ModInt) = plus(other.value)
+    operator fun plus(other: Int) = from(value + other)
+    operator fun minus(other: ModInt) = minus(other.value)
+    operator fun minus(other: Int) = from(value - other)
+    operator fun times(other: ModInt) = times(other.value)
+    operator fun times(other: Int) = from(value.toLong() * other)
+    operator fun div(other: ModInt) = times(other.inv())
+    operator fun div(other: Int) = div(from(other))
+
+    fun pow(exponent: Int): ModInt {
+        var ans = One
+        var a = this
+        var b = exponent
+        while (b > 0) {
+            if (b % 2 == 1) ans *= a
+            a *= a
+            b /= 2
+        }
+        return ans
+    }
+
+    fun inv(): ModInt = pow(MOD - 2)
+
+    override fun toString() = value.toString()
+
+    companion object {
+        fun from(value: Int) = ModInt(value.mod())
+        fun from(value: Long) = ModInt(value.mod())
+        fun Int.mod() = mod(MOD)
+        fun Long.mod() = mod(MOD)
+        val Zero = from(0)
+        val One = from(1)
+        const val MOD = 1000000007
+    }
+}
+
 class ModCombination(n: Int) {
     val factorial = Array(n + 1) { ModInt.One }.apply {
         for (i in 1..n) this[i] = this[i - 1] * i
